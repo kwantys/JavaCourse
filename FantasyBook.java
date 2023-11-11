84f389edc284435da7aa55b1af70584fa0ed79d0
kwantys <114281536+kwantys@users.noreply.github.com>
2023-11-11 14:42:44 +0300

    Add files via upload

---
+++ FantasyBook.java
@@ -0,0 +1,17 @@
+package bookshelf.bookstore;
+
+/**
+ *
+ * @author lenovo
+ */
+public class FantasyBook extends Book {
+    
+    FantasyBook(){
+        this.genre = "Fantasy";
+    }
+    
+    @Override
+    public void printContent(){
+        System.out.println(this.getISBN()+":"+this.getGenre()+","+this.getPages()+"pp");
+    }
+}
---
+++ Genertor.java
@@ -0,0 +1,18 @@
+package bookshelf;
+
+import bookshelf.bookstore.Book;
+import bookshelf.bookstore.BookCash;
+import java.util.Random;
+
+public class Genertor {
+    static Book createBook(){
+        BookCash.loadCache();
+        Random rand = new Random();
+        int max = 9999, min = 1111;
+        int id = rand.nextInt(3 - 1 + 1) + 1;
+        Book b = BookCash.getBook(id);
+        b.setISBN(rand.nextInt(max - min + 1) + min);
+        b.setPages(rand.nextInt(max/90 - min/300 + 1) + min/300);
+        return b;
+    }
+}
