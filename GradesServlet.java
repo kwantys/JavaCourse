2043f154f0a63602e3accc78c412c208b38a6eb1
kwantys <114281536+kwantys@users.noreply.github.com>
2023-12-25 18:16:23 +0300

    Update GradesServlet.java

--- GradesServlet.java
+++ GradesServlet.java
@@ -22,10 +22,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author tanka
- */
+
 @WebServlet(name = "GradesServlet", urlPatterns = {"/GradesServlet"})
 public class GradesServlet extends HttpServlet {
 
